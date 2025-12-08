edf1e1579f04ce4b38217183b52d0217be8197b5
pame1501 <patoapantapi@uide.edu.ec>
2025-12-08 12:49:30 +0000

    commitscript

--- script1.ps1
+++ script1.ps1
@@ -1,17 +1,21 @@
 function Start-ProgressBar
 {
-[CmdletBinding()]
-param
-(
-[Parameter (Mandatory = $true)]
-$Title,
-[Parameter (Mandatory = $true)]
-[int]$Timer
-)
-    For (\$ i = 1 $i le $Timer; $i++)
-{
-Start-Sleep -Seconds 1;
-Write-Progress -Activity $Title -Status "$i" -Percent
-Complete ($i/100 * 100)
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(Mandatory = $true)]
+        [string]$Title,
+
+        [Parameter(Mandatory = $true)]
+        [int]$Timer
+    )
+
+    for ($i = 1; $i -le $Timer; $i++)
+    {
+        Start-Sleep -Seconds 1
+
+        $percent = [int](($i / $Timer) * 100)
+
+        Write-Progress -Activity $Title -Status "$i" -PercentComplete $percent
+    }
 }
-} #Function Start-ProgressBar
